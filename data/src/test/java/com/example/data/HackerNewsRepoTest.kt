2292a24198062a210ca2a26d71e2d1f6992d1c6f
Gyongun Min <mindev93@foodfly.co.kr>
2019-02-26 16:59:24 +0900

    mockito 제거

--- data/src/test/java/com/example/data/HackerNewsRepoTest.kt
+++ data/src/test/java/com/example/data/HackerNewsRepoTest.kt
@@ -1,15 +1,6 @@
 package com.example.data
 
 import com.example.data.api.HackerNewsApi
-import com.example.data.entities.DataEntity
-import com.example.data.mapper.DataHackerNewsMapper
-import com.example.data.repository.HackerNewsRepositoryImpl
-import com.example.data.source.news.remote.HackerNewsRemoteDataSource
-import com.example.domain.entities.DomainEntity
-import com.nhaarman.mockitokotlin2.given
-import com.nhaarman.mockitokotlin2.mock
-import com.nhaarman.mockitokotlin2.verify
-import io.reactivex.Single
 import okhttp3.OkHttpClient
 import org.junit.Test
 import retrofit2.Retrofit
@@ -21,77 +12,16 @@ class HackerNewsRepoTest {
 
     @Test
     fun `check, remote data values`() {
-        val list: MutableList<DataEntity.HackerNews> = mutableListOf()
-        list.apply {
-            add(DataEntity.HackerNews(0, "1", 0, 0, 0, "1", "1", "1", "1", "1"))
-            add(DataEntity.HackerNews(0, "1", 0, 0, 0, "1", "1", "1", "1", "1"))
-            add(DataEntity.HackerNews(0, "1", 0, 0, 0, "1", "1", "1", "1", "1"))
-            add(DataEntity.HackerNews(0, "1", 0, 0, 0, "1", "1", "1", "1", "1"))
-        }
-
-        val hackerNewsRemoteDataSource: HackerNewsRemoteDataSource = mock()
-        given { hackerNewsRemoteDataSource.getHackerNewsList(3) }.willReturn(Single.just(list))
-
-        hackerNewsRemoteDataSource.getHackerNewsList(3)
-            .test()
-            .assertNoErrors()
-            .assertComplete()
-            .assertValue(list)
-
-        verify(hackerNewsRemoteDataSource).getHackerNewsList(3)
     }
 
     @Test
     fun `check, DataEntity change DomainEntity`() {
-        val list: ArrayList<DataEntity.HackerNews> = ArrayList()
-        list.apply {
-            add(DataEntity.HackerNews(0, "1", 0, 0, 0, "1", "1", "1", "1", "1"))
-            add(DataEntity.HackerNews(0, "1", 0, 0, 0, "1", "1", "1", "1", "1"))
-            add(DataEntity.HackerNews(0, "1", 0, 0, 0, "1", "1", "1", "1", "1"))
-            add(DataEntity.HackerNews(0, "1", 0, 0, 0, "1", "1", "1", "1", "1"))
-        }
 
-        val hackerNewsRemoteDataSource: HackerNewsRemoteDataSource = mock()
-        given { hackerNewsRemoteDataSource.getHackerNewsList(3) }.willReturn(Single.just(list))
-
-        val list2: ArrayList<DomainEntity.HackerNews> = ArrayList()
-        list2.apply {
-            add(DomainEntity.HackerNews(0, 0, "1", "1"))
-            add(DomainEntity.HackerNews(0, 0, "1", "1"))
-            add(DomainEntity.HackerNews(0, 0, "1", "1"))
-            add(DomainEntity.HackerNews(0, 0, "1", "1"))
-        }
-
-        val hackerNewsMapper = DataHackerNewsMapper()
-        hackerNewsRemoteDataSource.getHackerNewsList(3)
-            .map { hackerNewsMapper.mapFromEntity(it) }
-            .test()
-            .assertNoErrors()
-            .assertComplete()
-            .assertValue(list2)
-
-        verify(hackerNewsRemoteDataSource).getHackerNewsList(3)
     }
 
     @Test
     fun `check, repository data`() {
-        val list: MutableList<DomainEntity.HackerNews> = mutableListOf()
-        list.apply {
-            add(DomainEntity.HackerNews(0, 0, "1"))
-            add(DomainEntity.HackerNews(0, 0, "1"))
-            add(DomainEntity.HackerNews(0, 0, "1"))
-            add(DomainEntity.HackerNews(0, 0, "1"))
-        }
 
-        val hackerNewsRepositoryImpl: HackerNewsRepositoryImpl = mock()
-        given { hackerNewsRepositoryImpl.getHackerNewsList(3) }.willReturn(Single.just(list))
-        hackerNewsRepositoryImpl.getHackerNewsList(3)
-            .test()
-            .assertNoErrors()
-            .assertComplete()
-            .assertValue(list)
-
-        verify(hackerNewsRepositoryImpl).getHackerNewsList(3)
     }
 
     @Test
--- domain/src/test/java/com/example/domain/HackerNewsUseCaseTest.kt
+++ domain/src/test/java/com/example/domain/HackerNewsUseCaseTest.kt
@@ -1,32 +1,10 @@
 package com.example.domain
 
-import com.example.domain.entities.DomainEntity
-import com.example.domain.usecase.news.HackerNewsUseCase
-import com.nhaarman.mockitokotlin2.mock
-import com.nhaarman.mockitokotlin2.verify
-import io.reactivex.Single
 import org.junit.Test
-import org.mockito.Mockito.`when`
 
 class HackerNewsUseCaseTest {
     @Test
     fun `hacker news, useCase execute`() {
-        val mock = mock<HackerNewsUseCase>()
-        val list = ArrayList<DomainEntity.HackerNews>()
-        list.apply {
-            add(DomainEntity.HackerNews(0, 0, "1", "1"))
-            add(DomainEntity.HackerNews(0, 0, "1", "1"))
-            add(DomainEntity.HackerNews(0, 0, "1", "1"))
-            add(DomainEntity.HackerNews(0, 0, "1", "1"))
-        }
-        `when`(mock.execute(HackerNewsUseCase.Param(3))).thenReturn(Single.just(list))
 
-        mock.execute(HackerNewsUseCase.Param(3))
-            .test()
-            .assertNoErrors()
-            .assertComplete()
-            .assertValue(list)
-
-        verify(mock).execute(HackerNewsUseCase.Param(3))
     }
 }
