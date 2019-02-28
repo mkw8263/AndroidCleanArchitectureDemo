132fe99d4f4e23d9537acc8fcabd4b2a30778983
Gyongun Min <mindev93@foodfly.co.kr>
2019-02-28 12:00:04 +0900

    presentation2 제거

--- presentation2/src/main/java/com/example/mindevandroidcleanarchitecturedemo/di/qualifier/ApplicationContext.kt
+++
@@ -1,7 +0,0 @@
-package com.example.mindevandroidcleanarchitecturedemo.di.qualifier
-
-import javax.inject.Qualifier
-
-@Qualifier
-@Retention(AnnotationRetention.RUNTIME)
-annotation class ApplicationContext
--- presentation2/src/main/java/com/example/mindevandroidcleanarchitecturedemo/di/qualifier/PerActivity.kt
+++
@@ -1,7 +0,0 @@
-package com.example.mindevandroidcleanarchitecturedemo.di.qualifier
-
-import javax.inject.Scope
-
-@Scope
-@Retention(AnnotationRetention.RUNTIME)
-annotation class PerActivity
